d3c50a32fc3a955e810c4aa0021c2547ce820bc1
jalsol <jalsol@pm.me>
2025-02-13 00:12:00 +0700

    Add server logging

--- lib/server.ml
+++ lib/server.ml
@@ -6,32 +6,43 @@ let backlog = 1
 let create_socket port =
     let sock = Lwt_unix.(socket PF_INET SOCK_STREAM 0) in
     let sockaddr = Lwt_unix.ADDR_INET (Unix.inet_addr_any, port) in
-    let _ = Lwt_unix.bind sock sockaddr in
-    Lwt_unix.listen sock backlog;
+    Lwt_main.run (
+        let* () = Lwt_unix.bind sock sockaddr in
+        Lwt_unix.listen sock backlog;
+        Lwt.return_unit
+    );
     sock
 
 let handle_message msg =
     Printf.sprintf "User typed: %s" msg
 
-let rec handle_connection in_chan out_chan =
+let string_of_sockaddr = function
+    | Unix.ADDR_INET (addr, port) ->
+        Printf.sprintf "%s:%d" (Unix.string_of_inet_addr addr) port
+    | Unix.ADDR_UNIX path ->
+        path
+
+let rec handle_connection sockaddr in_chan out_chan =
     let* msg = Lwt_io.read_line_opt in_chan in
     match msg with
     | Some msg ->
+        Logs.app (fun m -> m "%s: %s" (string_of_sockaddr sockaddr) msg);
         let reply = handle_message msg in
-        let _ = Lwt_io.write_line out_chan reply in
-        handle_connection in_chan out_chan
+        let* () = Lwt_io.write_line out_chan reply in
+        handle_connection sockaddr in_chan out_chan
     | None ->
-        Logs_lwt.info (fun m -> m "Connection closed") 
-
-let fail_callback error =
-    Logs.err (fun m -> m "%s" (Printexc.to_string error))
+        Logs_lwt.info (fun m -> m "Connection closed: %s" (string_of_sockaddr sockaddr)) 
 
 let accept_connection conn =
-    let (client_sock, _) = conn in
+    let (client_sock, client_sockaddr) = conn in
     let in_chan = Lwt_io.(of_fd ~mode:Input client_sock) in
     let out_chan = Lwt_io.(of_fd ~mode:Output client_sock) in
-    Lwt.on_failure (handle_connection in_chan out_chan) fail_callback;
-    Logs_lwt.info (fun m -> m "New connection") >>= Lwt.return
+    Lwt.on_failure
+        (handle_connection client_sockaddr in_chan out_chan)
+        (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e)));
+    let* () = Logs_lwt.info
+        (fun m -> m "New connection: %s" (string_of_sockaddr client_sockaddr)) in
+    Lwt.return_unit
 
 let create_server sock =
     let rec serve () =
@@ -41,6 +52,9 @@ let create_server sock =
     in serve
 
 let run_server port =
+    Logs.set_level (Some Logs.Info);
+    Logs.set_reporter (Logs.format_reporter ());
+    Logs.info (fun m -> m "Starting server on port %d" port);
     let sock = create_socket port in
     let serve = create_server sock in
     Lwt_main.run @@ serve ()
